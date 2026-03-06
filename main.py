8cf3bc66b76a14a7dc7a3ab1793105e5a62b2901
Leonardo-gabsantos <leog.santos2023@gmail.com>
2026-03-06 08:47:11 -0300

    Adicionado botão "Início"

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 import telebot
 import os
-from dotenv import load_dotenv  
+from dotenv import load_dotenv
+from telebot import types # Import necessário para os botões
 
 load_dotenv()
 
 TOKEN = os.getenv('TOKEN_BOT')
-
 bot = telebot.TeleBot(TOKEN)
-@bot.message_handler(['start', 'help'])
-def start(msg:telebot.types.Message):
-    bot.reply_to(msg, 'Olá, sou o bot gotinha a sua disposição!')
 
+
+@bot.message_handler(commands=['start', 'help'])
+def start(msg: telebot.types.Message):
+    
+    markup = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+    
+    
+    btn_inicio = types.KeyboardButton('Início')
+    
+    
+    markup.add(btn_inicio)
+    
+    bot.send_message(msg.chat.id, 'Olá, sou o bot gotinha a sua disposição! Clique no botão abaixo para começar.', reply_markup=markup)
+
+
+@bot.message_handler(func=lambda msg: msg.text == "Início")
+def resposta_inicio(msg):
+    bot.reply_to(msg, "Você voltou ao início! Como posso te ajudar com as vacinas hoje?")
 
 bot.infinity_polling()
